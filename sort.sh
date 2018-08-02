85ce720f2212f921128d5fd2553cc38e16663c66
gjskha <gjskha@gmail.com>
2018-08-01 17:36:41 -0700

    added commentary

--- sort.sh
+++ sort.sh
@@ -77,31 +77,37 @@ function insertionsort() {
     # weird hacky stuff to pass array in bash
     name=$1[@]
     the_array=("${!name}")
-    n=$2
+    array_length=$2
     
-    i=1; 
-    j=0; 
-    key=0;
+    this_iteration=1; 
+    next_cell_down=0; 
+    #this_iterations_value=0;
 
-    while [ $i -lt $n ]; do
+    while [ $this_iteration -lt $array_length ]; do
     
-        key=${the_array[$i]};
-	j=$(($i - 1));
+        this_iterations_value=${the_array[$this_iteration]};
+	next_cell_down=$(($this_iteration - 1));
 
-	# move elements of array less than i that are greater than key 
+        echo "Iteration $this_iteration of $array_length. Value of $this_iterations_value"
+
+	# move elements of array less than this_iteration that are greater than this_iterations_value 
 	# one position ahead of their current position
-	while [ $j -ge 0 ] && [ ${the_array[$j]} -gt $key ] ; do
+        # convert to if/else for clarity
+	while [ $next_cell_down -ge 0 ] && [ ${the_array[$next_cell_down]} -lt $this_iterations_value ] ; do
+	#while [ $next_cell_down -ge 0 ] && [ ${the_array[$next_cell_down]} -lt ${the_array[$this_iteration]} ] ; do
       
-            k=$(($j + 1))
+            echo "at outer iteration $this_iteration and inner iteration $next_cell_down : $this_iterations_value is greater than ${the_array[$next_cell_down]}"
+
+            k=$(($next_cell_down + 1))
 	    tmp=${the_array[$k]}
 	    
-	    the_array[$k]=${the_array[$j]}
-	    the_array[$j]=$tmp
+	    the_array[$k]=${the_array[$next_cell_down]}
+	    the_array[$next_cell_down]=$tmp
 
-	    j=$(($j - 1))  
+	    next_cell_down=$(($next_cell_down - 1))  
 	done
 
-        i=$(( $i + 1 ))	
+        this_iteration=$(( $this_iteration + 1 ))	
     done
 
     echo -n "After sorting the array:"
@@ -113,3 +119,13 @@ function insertionsort() {
 }
 
 insertionsort the_array 8
+
+declare -a the_array=(10 9 8 7 6 5 4 3 2 1 11)
+
+echo -n "Starting with the array:"
+for elem in "${the_array[@]}" ; do
+     echo -n "$elem "
+done
+echo 
+
+insertionsort the_array 11
