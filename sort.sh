4cf429e49635aa41f3b92092cfdb0f3c43de2743
gjskha <gjskha@gmail.com>
2018-08-01 17:03:15 -0700

    add insertion sort

--- sort.sh
+++ sort.sh
@@ -1,3 +1,5 @@
+echo "Bubble Sort"
+
 declare -a the_array=(8 4 2 1 7 11 )
 
 echo -n "Starting with the array:"
@@ -58,3 +60,57 @@ function bubblesort() {
 }
 
 bubblesort the_array 6
+
+echo
+echo "Insertion Sort"
+
+declare -a the_array=(11 42 2 55 6 13 1 22)
+
+echo -n "Starting with the array:"
+for elem in "${the_array[@]}" ; do
+     echo -n "$elem "
+done
+echo 
+
+function insertionsort() {
+
+    # weird hacky stuff to pass array in bash
+    name=$1[@]
+    the_array=("${!name}")
+    n=$2
+    
+    i=1; 
+    j=0; 
+    key=0;
+
+    while [ $i -lt $n ]; do
+    
+        key=${the_array[$i]};
+	j=$(($i - 1));
+
+	# move elements of array less than i that are greater than key 
+	# one position ahead of their current position
+	while [ $j -ge 0 ] && [ ${the_array[$j]} -gt $key ] ; do
+      
+            k=$(($j + 1))
+	    tmp=${the_array[$k]}
+	    
+	    the_array[$k]=${the_array[$j]}
+	    the_array[$j]=$tmp
+
+	    j=$(($j - 1))  
+	done
+
+        i=$(( $i + 1 ))	
+    done
+
+echo -n "After sorting the array:"
+for elem in "${the_array[@]}" ; do
+     echo -n "$elem "
+done
+echo 
+
+
+}
+
+insertionsort the_array 8
